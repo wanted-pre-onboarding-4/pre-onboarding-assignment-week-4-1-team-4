059f171a525bfe253373d85121d0cbc90f40df89
이시형 <tlgud585@gmail.com>
2022-09-21 05:51:55 +0900

    fix : api 요청 함수 수정 axios create로 생성한 instance를 사용하여 api 요청

--- client/src/services/auth.js
+++ client/src/services/auth.js
@@ -1,15 +1,9 @@
-import axios from "axios"
+import instance from '../utils/http-common';
 
 /** 이메일, 비밀번호를 인자값으로 받음*/
-export const login = async(email, password)=>{
-  try{
-    const res = await axios.post("/login",{
-      "email" : email,
-      "password" : password
-    })
-    return res
-  }catch(err){
-    window.alert('존재하지 않는 아이디 입니다.')
-  }
-  
-}
+export const login = (email, password) => {
+  return instance.post('/login', {
+    email: email,
+    password: password,
+  });
+};
