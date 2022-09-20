87ef7c17f53f993faf7b8da352c9af5eeb632678
Jaehyeok <milkboy2564@naver.com>
2022-09-20 22:06:51 +0900

    feat: Global style 정의

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import GlobalStyle from './styles/Globalstyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Routes>
-        <Route path="/" element={<div>홈</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<div>로그인 페이지</div>} />
         <Route path="/users" element={<div>유저 목록 페이지</div>} />
         <Route path="/users/:user_id" element={<div>유저 상세 페이지</div>} />
---
+++ client/src/styles/Globalstyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+a {
+    text-decoration: none;
+    color: inherit;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    display: grid;
+  }
+`;
+
+export default GlobalStyle;
