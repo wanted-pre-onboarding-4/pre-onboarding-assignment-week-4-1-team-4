d2663e4dc8f5d146469448b6726e5606cdf3c70b
vjvl95 <vjvl95@naver.com>
2022-09-20 19:01:12 +0900

    feat : tailwind 적용

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import Home from './pages/home/home';
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>홈</div>} />
+        <Route path="/" element={<Home></Home>} />
         <Route path="/login" element={<div>로그인 페이지</div>} />
         <Route path="/users" element={<div>유저 목록 페이지</div>} />
         <Route path="/users/:user_id" element={<div>유저 상세 페이지</div>} />
---
+++ client/src/pages/home/home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <button className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+      Hello Tailwind
+    </button>
+  );
+}
+
+export default Home;
