abefe638ea5f4ae9472a8634babccc8b75fcd842
이시형 <tlgud585@gmail.com>
2022-09-20 23:48:56 +0900

    feat : context api 추가 auth 관리하는 context 생성및 index.js 파일에 Provider 추가

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./index.css"
+import AuthProvider from './contexts/AuthProvider';
+import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
