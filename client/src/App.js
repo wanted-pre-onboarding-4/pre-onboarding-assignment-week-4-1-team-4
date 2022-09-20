a728f22d6a432d24f36557bef6af67e57864898b
Jaehyeok <milkboy2564@naver.com>
2022-09-20 23:52:22 +0900

    fix: conflict Master 병합에 따른 충돌 해결

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login/Login';
 import GlobalStyle from './styles/Globalstyles';
+import { getToken } from './utils/func';
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<div>로그인 페이지</div>} />
+        <Route
+          path="/"
+          element={getToken() ? <Navigate to="/users" /> : <Navigate to="/login" />}
+        />
+        <Route path="/login" element={<Login />} />
         <Route path="/users" element={<div>유저 목록 페이지</div>} />
         <Route path="/users/:user_id" element={<div>유저 상세 페이지</div>} />
         <Route path="/accounts" element={<div>계좌 목록 페이지</div>} />
--- client/src/styles/Globalstyles.js
+++ client/src/styles/Globalstyles.js
@@ -2,14 +2,14 @@ import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-  ${reset};
+${reset};
 a {
     text-decoration: none;
     color: inherit;
   }
-  * {
+* {
     box-sizing: border-box;
-  }
+}
   body {
     display: grid;
   }
