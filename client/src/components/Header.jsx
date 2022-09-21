f64fd835ebfe9bdcc1cdf1d39bb83d84fe47d78e
김명원 <auddnjs2008@naver.com>
2022-09-21 15:21:01 +0900

    feat: logout 기능 구현 context 의 userInfo 와  token 삭제 함으로써 logout 기능 구현

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 import { UserContext } from '../contexts/user';
+import token from '../utils/token';
 
 const Header = () => {
-  const { user } = useContext(UserContext);
+  const { user, setUserInfo } = useContext(UserContext);
+
+  const logOutClick = useCallback(() => {
+    setUserInfo({ email: '', id: null });
+    token.remove();
+  }, []);
+
   return (
     <header className="w-[85vw] backdrop-blur-sm grid grid-cols-2 ml-48 px-12 py-5 items-center fixed top-0 ">
       <h1 className="text-2xl font-bold text-slate-700">투자계좌</h1>
-      <h5 className="justify-self-end ">{user.email}</h5>
+      <div className="flex justify-self-end items-center">
+        <h5 className="mr-5">{user.email}</h5>
+        <button
+          onClick={logOutClick}
+          className="rounded-sm bg-slate-500 text-white py-2 px-2 active:scale-95"
+        >
+          LogOut
+        </button>
+      </div>
     </header>
   );
 };
--- client/src/utils/token.js
+++ client/src/utils/token.js
@@ -1,6 +1,7 @@
 const token = {
   get: () => localStorage.getItem('adminToken'),
   set: (key, value) => localStorage.setItem(key, value),
+  remove: () => localStorage.removeItem('adminToken'),
 };
 
 export default token;
