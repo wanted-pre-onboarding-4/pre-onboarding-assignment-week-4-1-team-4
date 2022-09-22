0e9ea3e1e7ae20e3f442d749f45fa1a087a1e038
kyungbaa <cocoroto@naver.com>
2022-09-22 13:55:28 +0900

    style: login페이지 레이아웃 수정 일부 디테일 수정

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { BsPerson } from 'react-icons/bs';
-import { AiOutlineLock } from 'react-icons/ai';
+import { AiOutlineLock, AiOutlineMail } from 'react-icons/ai';
 import { BiLogIn } from 'react-icons/bi';
+
 import authStore from '../../store/AuthStore';
 
 const Login = () => {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
 
-  const {onLogin } = authStore();
+  const { onLogin } = authStore();
 
   const changeEmail = e => {
     setEnteredEmail(e.target.value);
@@ -30,34 +31,38 @@ const Login = () => {
 
   return (
     <section className="flex flex-col justify-center items-center w-full gap-y-6">
-      <h1 className="text-5xl font-extrabold text-blue-900 mb-8">PREFACE</h1>
-      <div className="border-solid border-[1px] rounded-md shadow-md shadow-gray-300 w-1/2">
-        <div className="flex items-center justify-start space-x-2 p-4 font-medium text-xl bg-gray-100">
+      <h1 className="text-5xl font-extrabold text-blue-900 mb-3">PREFACE</h1>
+      <div className="border-solid border-[1px] rounded-md shadow-sm shadow-gray-300 w-1/3">
+        <div className="flex justify-start items-center space-x-2 p-4 pl-8 font-medium text-xl bg-gray-100">
           <BsPerson />
           <h2>로그인</h2>
         </div>
-        <form onSubmit={loginHandler} className="flex flex-col gap-y-8 p-4">
-          <label className="flex items-center space-x-2 border-solid border-[1px] p-2 rounded">
-            <BsPerson />
+        <form onSubmit={loginHandler} className="flex flex-col gap-y-4 p-6 pt-4">
+          <div className="mb-4">
+            <label className="flex items-center block text-gray-700 text-sm font-bold mb-2"></label>
+            <AiOutlineMail className="mr-2 absolute mt-3.5 ml-3.5 text-lg" />
             <input
-              type="email"
+              type="text"
               value={enteredEmail}
               onChange={changeEmail}
               placeholder="이메일을 입력하세요"
-              className="outline-none p-1"
+              class="mt-1 block w-full px-10 py-3  bg-white  border-solid border-[1px]  rounded text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-blue-500 focus:ring-1"
             />
-          </label>
-          <label className="flex items-center space-x-2 border-solid border-[1px] p-2 rounded">
-            <AiOutlineLock />
+
+            <label className="flex items-center block text-gray-700 text-sm font-bold mb-2"></label>
+            <AiOutlineLock className="mr-2 absolute mt-3.5 ml-3.5 text-lg " />
             <input
               type="password"
               autoComplete="off"
               value={enteredPassword}
               onChange={changePassword}
               placeholder="비밀번호를 입력하세요"
-              className="outline-none p-1"
+              class="mt-1 block w-full px-10 py-3 bg-white  border-solid border-[1px]  rounded text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-blue-500 focus:ring-1"
             />
-          </label>
+          </div>
+
           <button
             className="flex items-center justify-center space-x-2 w-full text-lg border-solid border-[1px] p-2 rounded text-white bg-blue-900 duration-200 disabled:text-gray-400 disabled:bg-gray-100"
             disabled={!formValid}
