866ed8c50c2eb106c44fd6beaf88e9bcc134f561
kyungbaa <cocoroto@naver.com>
2022-09-23 21:28:16 +0900

    style: 사용하지 않는 메뉴 삭제 대시보드 삭제

--- client/src/components/Sider.jsx
+++ client/src/components/Sider.jsx
@@ -1,28 +1,20 @@
 import React, { useState } from 'react';
-import { AiOutlineDashboard } from 'react-icons/ai';
-import { MdOutlineAccountBalance, MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
-import { IoMdAnalytics, IoMdLogOut } from 'react-icons/io';
+import { MdOutlineAccountBalance } from 'react-icons/md';
+import { IoMdLogOut } from 'react-icons/io';
 import { BsPerson } from 'react-icons/bs';
 import { NavLink } from 'react-router-dom';
 import authStore from '../store/AuthStore';
-import { useLocation, useParams } from 'react-router-dom/dist';
+
 // import { useAuth } from '../contexts/AuthProvider';
 const Sider = () => {
-  const [openSubCategory, setOpenSubCategory] = useState(false);
+  const [, setOpenSubCategory] = useState(false);
 
   const { onLogout } = authStore();
-
-  const { pathname } = useLocation();
-  const { account_id } = useParams();
 
   return (
     <div className="w-52 h-screen  fixed left-0 bottom-0 bg-neutral-800 border-t-4 border-blue-600">
       <h1 className="text-3xl font-extrabold text-center text-white px-5 py-4 pr-7">PREFACE</h1>
-      <ul>
-        <li className="flex items-center px-8 space-x-2 py-4 text-gray-300  hover:text-white ease-in duration-100">
-          <AiOutlineDashboard />
-          <span>대시보드</span>
-        </li>
+      <ul className="mt-7">
         <li
           onMouseOver={() => setOpenSubCategory(true)}
           onMouseLeave={() => setOpenSubCategory(false)}
@@ -37,26 +29,8 @@ const Sider = () => {
               <MdOutlineAccountBalance />
               <span>계좌 목록</span>
             </div>
-            <div>
-              {openSubCategory ? (
-                <MdKeyboardArrowUp className="fill-gray-300 text-xl" />
-              ) : (
-                <MdKeyboardArrowDown className="fill-gray-300 text-xl" />
-              )}
-            </div>
           </NavLink>
-          {(pathname === `/accounts/${account_id}` || openSubCategory) && (
-            <NavLink
-              to="/accounts/:id"
-              className="flex items-center px-10 space-x-2 py-3 text-gray-300 hover:text-white ease-in duration-100 "
-              onClick={() => setOpenSubCategory(true)}
-            >
-              <IoMdAnalytics />
-              <span>투자 계좌</span>
-            </NavLink>
-          )}
         </li>
-
         <li>
           <NavLink
             to="/users"
